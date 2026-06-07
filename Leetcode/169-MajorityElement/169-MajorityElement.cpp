3f96dcb5eadaad88126b8b66629e2f8250e4c46d
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-06-07 23:26:41 +0530

    Create 169-MajorityElement

---
+++ Leetcode/169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/7/2026, 11:26:40 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        for(int i =0;i<n;i++)
+        {
+            freq[nums[i]]++;
+            if(freq[nums[i]]>n/2)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
