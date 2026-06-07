209cbdcccb95fd42a6a3dde826d7d8960273f2d8
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-06-07 23:27:42 +0530

    Create 55-JumpGame

---
+++ Leetcode/55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/7/2026, 11:27:41 PM
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n =nums.size();
+        int last = n-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i+nums[i]>=last)
+            {
+                last=i;
+            }
+        }
+        return last==0;
+    }
+};
