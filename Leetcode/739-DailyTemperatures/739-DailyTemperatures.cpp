7f45a312016548b8b19596605add106533933010
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-06-07 23:25:44 +0530

    Create 739-DailyTemperatures

---
+++ Leetcode/739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/7/2026, 11:25:44 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty()&&temperatures[i]>=temperatures[st.top()]){
+                st.pop();
+            }
+            ans[i]=st.empty()?0:(st.top()-i);
+            st.push(i);
+        }
+        return ans;
+    }
+};
