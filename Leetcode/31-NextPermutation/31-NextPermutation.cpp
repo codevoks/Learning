6e46271057418cd110d8124dce889fff51b32b55
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-06-07 23:27:51 +0530

    Create 31-NextPermutation

---
+++ Leetcode/31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,32 @@
+// Last updated: 6/7/2026, 11:27:50 PM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) 
+    {
+        int idx = -1;
+        int n = nums.size();
+        for(int i = n-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1)
+        {
+            sort(nums.begin(),nums.end());
+            return;
+        }
+        int prev = idx;
+        for(int i = idx+1;i<n;i++)
+        {
+            if(nums[i]>nums[idx-1]&&nums[i]<=nums[prev])
+            {
+                prev = i;
+            }
+        }
+        swap(nums[idx-1],nums[prev]);
+        sort(nums.begin()+idx,nums.end());
+    }
+};
