4cca20007b78d33041848bbc4570d3389fc68921
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-09-13 09:00:41 +0530

    Create 1048-LongestStringChain

---
+++ Leetcode/1048-LongestStringChain/1048-LongestStringChain.cpp
@@ -0,0 +1,44 @@
+// Last updated: 9/13/2026, 9:00:40 AM
+class Solution {
+public:
+    static int compare(const string& a, const string& b){
+        return a.size()<b.size();
+    }
+    int isPredecessor(string a,string b){
+        int l1=a.size();
+        int l2=b.size();
+        if(l1+1!=l2){
+            return 0;
+        }
+        int i=0,j=0;
+        while(i<l1&&j<l2){
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return i==l1;
+    }
+    int longestStrChain(vector<string>& words) {
+        //dp[i] - length of lis ending at i
+        sort(words.begin(),words.end(),compare);
+        int n=words.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(isPredecessor(words[j],words[i])){
+                    if(dp[j]+1>dp[i]){
+                        dp[i]=dp[j]+1;
+                    }
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
