0efb85728efd9b13c860edbab421c2caebfee9e4
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-09-13 09:01:35 +0530

    Create 901-OnlineStockSpan

---
+++ Leetcode/901-OnlineStockSpan/901-OnlineStockSpan.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/13/2026, 9:01:35 AM
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+        st = stack<pair<int,int>>();
+    }
+    
+    int next(int price) {
+        int span = 1;
+        while(!st.empty()&&(st.top().first<=price)){
+            span+=st.top().second;
+            st.pop();
+        }
+        st.push({price,span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
