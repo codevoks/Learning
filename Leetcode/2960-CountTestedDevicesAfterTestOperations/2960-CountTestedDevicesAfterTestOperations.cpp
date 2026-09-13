1d575ba066321d4efe200a599652598deea2a8e4
Vansh Kumar Singh <vanshkumar841@gmail.com>
2026-09-13 08:59:02 +0530

    Create 2960-CountTestedDevicesAfterTestOperations

---
+++ Leetcode/2960-CountTestedDevicesAfterTestOperations/2960-CountTestedDevicesAfterTestOperations.cpp
@@ -0,0 +1,20 @@
+// Last updated: 9/13/2026, 8:59:01 AM
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int n = batteryPercentages.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++)
+        {
+            if(batteryPercentages[i]>0)
+            {
+                ans++;
+                for(int j = i+1;j<n;j++)
+                {
+                    batteryPercentages[j]=max(0,batteryPercentages[j]-1);
+                }
+            }
+        }
+        return ans;
+    }
+};
